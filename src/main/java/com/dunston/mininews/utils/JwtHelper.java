57f8a529454cee31aafc2cbc37ce7406486e1b44
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-30 19:02:50 +0800

    引入JwtHelper工具类

---
+++ src/main/java/com/dunston/mininews/utils/JwtHelper.java
@@ -0,0 +1,55 @@
+package com.dunston.mininews.utils;
+
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.compression.CompressionCodecs;
+
+import java.util.Date;
+
+public class JwtHelper {
+    private static long tokenExpiration = 24*60*60*1000;
+    private static String tokenSignKey = "123456";
+
+    //生成token字符串
+    public static String createToken(Long userId) {
+        String token = Jwts.builder()
+
+                .setSubject("YYGH-USER")
+                .setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))
+                .claim("userId", userId)
+                .signWith(SignatureAlgorithm.HS512, tokenSignKey)
+                .compressWith(CompressionCodecs.GZIP)
+                .compact();
+        return token;
+    }
+
+    //从token字符串获取userid
+    public static Long getUserId(String token) {
+        if(StringUtils.isEmpty(token)) return null;
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+        Claims claims = claimsJws.getBody();
+        Integer userId = (Integer)claims.get("userId");
+        return userId.longValue();
+    }
+
+
+
+    //判断token是否有效
+    public static boolean isExpiration(String token){
+        try {
+            boolean isExpire = Jwts.parser()
+                    .setSigningKey(tokenSignKey)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getExpiration().before(new Date());
+            //没有过期，有效，返回false
+            return isExpire;
+        }catch(Exception e) {
+            //过期出现异常，返回true
+            return true;
+        }
+    }
+}

3a4eed89277a33d8a1498d9c6ce28580fa578791
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-30 21:02:33 +0800

    增加序列化id，完善注释

--- src/main/java/com/dunston/mininews/domain/request/NewsUserLoginRequest.java
+++ src/main/java/com/dunston/mininews/domain/request/NewsUserLoginRequest.java
@@ -2,13 +2,16 @@ package com.dunston.mininews.domain.request;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
+/**
+ * 用户登录请求封装类
+ *
+ * @author dunston
+ */
 @Data
-public class NewsUserLoginRequest {
+public class NewsUserLoginRequest implements Serializable {
+    private static final long serialVersionUID = 31912417163120793L;
     private String username;
     private String userPwd;
-
-    public NewsUserLoginRequest(String username, String userPwd) {
-        this.username = username;
-        this.userPwd = userPwd;
-    }
 }

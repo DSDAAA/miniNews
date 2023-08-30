a6fd9be805f005ba06512ed9f4c37e826b154f8d
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-30 21:05:12 +0800

    实现用户登录

--- src/main/java/com/dunston/mininews/controller/NewsUserController.java
+++ src/main/java/com/dunston/mininews/controller/NewsUserController.java
@@ -1,19 +1,45 @@
 package com.dunston.mininews.controller;
 
+import com.dunston.mininews.common.Result;
 import com.dunston.mininews.domain.NewsUser;
+import com.dunston.mininews.domain.request.NewsUserLoginRequest;
 import com.dunston.mininews.service.NewsUserService;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * 用户控制层
+ *
+ * @author dunston
+ */
 @RestController
+@RequestMapping("/user")
 public class NewsUserController {
     @Resource
     NewsUserService newsUserService;
 
+    /**
+     * 用户登录
+     *
+     * @param userLoginRequest
+     * @param request
+     * @return
+     */
     @PostMapping("/login")
-    public String login(@RequestBody NewsUser newUser) {
-        return "";
+    public Result<NewsUser> login(@RequestBody NewsUserLoginRequest userLoginRequest, HttpServletRequest request) {
+        //1.判断是否为空
+        if (userLoginRequest == null) {
+            return null;
+        }
+        String username = userLoginRequest.getUsername();
+        String password = userLoginRequest.getUserPwd();
+        //2.用户名和密码任意判断为空
+        if (StringUtils.isAnyBlank(username, password)) {
+            return null;
+        }
+        NewsUser result = newsUserService.login(username, password, request);
+        return Result.ok(result);
     }
 }

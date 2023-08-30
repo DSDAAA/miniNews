afbac723a68b0b9440c3cf4eb61e8a8ee6f77bc4
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-30 18:34:24 +0800

    引入MybatisPlus项目MapperScan注解

--- src/main/java/com/dunston/mininews/MininewsApplication.java
+++ src/main/java/com/dunston/mininews/MininewsApplication.java
@@ -1,9 +1,11 @@
 package com.dunston.mininews;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.dunston.mininews.mapper")
 public class MininewsApplication {
 
     public static void main(String[] args) {

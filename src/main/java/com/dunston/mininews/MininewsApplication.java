fa5c3aa4151eddded664f239b7278eef9cf25993
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-30 18:13:56 +0800

    项目初始化

---
+++ src/main/java/com/dunston/mininews/MininewsApplication.java
@@ -0,0 +1,13 @@
+package com.dunston.mininews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MininewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MininewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dunston/mininews/MininewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunston.mininews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MininewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

bb0f42f654ee2ef2bee3c2d2c37c67ec5c01c618
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-30 18:53:53 +0800

    User接口

--- src/main/java/com/dunston/mininews/service/NewsUserService.java
+++ src/main/java/com/dunston/mininews/service/NewsUserService.java
@@ -2,12 +2,15 @@ package com.dunston.mininews.service;
 
 import com.dunston.mininews.domain.NewsUser;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.dunston.mininews.domain.request.NewsUserLoginRequest;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
-* @author 13180
-* @description 针对表【news_user】的数据库操作Service
-* @createDate 2023-08-30 18:35:08
-*/
+ * @author 13180
+ * @description 针对表【news_user】的数据库操作Service
+ * @createDate 2023-08-30 18:35:08
+ */
 public interface NewsUserService extends IService<NewsUser> {
-
+    NewsUser login(NewsUserLoginRequest newsUserLoginRequest);
 }

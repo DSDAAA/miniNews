28e3599db85679c6ee4d484ee2555a21ce54223b
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-31 16:03:26 +0800

    多余包删除

--- src/main/java/com/dunston/mininews/service/NewsUserService.java
+++ src/main/java/com/dunston/mininews/service/NewsUserService.java
@@ -2,8 +2,6 @@ package com.dunston.mininews.service;
 
 import com.dunston.mininews.domain.NewsUser;
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.dunston.mininews.domain.request.NewsUserLoginRequest;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 
 /**

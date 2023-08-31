a37a46cd43cca3d45fcf11fbfc277c3fe08f8b59
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-31 16:05:09 +0800

    获取新闻类型实现

--- src/main/java/com/dunston/mininews/service/impl/NewsTypeServiceImpl.java
+++ src/main/java/com/dunston/mininews/service/impl/NewsTypeServiceImpl.java
@@ -4,17 +4,34 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dunston.mininews.domain.NewsType;
 import com.dunston.mininews.service.NewsTypeService;
 import com.dunston.mininews.mapper.NewsTypeMapper;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
-* @author 13180
-* @description 针对表【news_type】的数据库操作Service实现
-* @createDate 2023-08-31 15:00:18
-*/
+ * @author duston
+ * @description 针对表【news_type】的数据库操作Service实现
+ * @createDate 2023-08-31 15:00:18
+ */
 @Service
 public class NewsTypeServiceImpl extends ServiceImpl<NewsTypeMapper, NewsType>
-    implements NewsTypeService{
+        implements NewsTypeService {
+    @Resource
+    NewsTypeMapper typeMapper;
 
+    /**
+     * 查询所有type
+     *
+     * @param request
+     * @return
+     */
+    @Override
+    public List<NewsType> findAllTypes(HttpServletRequest request) {
+        List<NewsType> newsTypes = typeMapper.selectList(null);
+        return newsTypes;
+    }
 }
 
 

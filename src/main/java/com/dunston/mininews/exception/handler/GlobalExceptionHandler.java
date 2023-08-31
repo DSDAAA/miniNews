dbf410a06d8ad7e379e7455f3349967f428ef61a
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-31 09:12:44 +0800

    增加全局异常处理类

---
+++ src/main/java/com/dunston/mininews/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.dunston.mininews.exception.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理类
+ *
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+}

30789551227c65d1cb9720fd253130dfb799ad75
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-31 16:03:44 +0800

    注释修改

--- src/main/java/com/dunston/mininews/mapper/NewsUserMapper.java
+++ src/main/java/com/dunston/mininews/mapper/NewsUserMapper.java
@@ -4,7 +4,7 @@ import com.dunston.mininews.domain.NewsUser;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
-* @author 13180
+* @author dunston
 * @description 针对表【news_user】的数据库操作Mapper
 * @createDate 2023-08-30 18:35:08
 * @Entity generator.domain.NewsUser

af1a247cb399430c783cec84756e0531d1e1284b
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-31 09:20:58 +0800

    增加系统错误信息描述40001 systemError

--- src/main/java/com/dunston/mininews/common/ResultCodeEnum.java
+++ src/main/java/com/dunston/mininews/common/ResultCodeEnum.java
@@ -7,21 +7,25 @@ package com.dunston.mininews.common;
  */
 public enum ResultCodeEnum {
 
-    SUCCESS(200,"success"),
-    USERNAME_ERROR(501,"usernameError"),
-    PASSWORD_ERROR(503,"passwordError"),
-    NOTLOGIN(504,"notLogin"),
-    USERNAME_USED(505,"userNameUsed");
+    SUCCESS(200, "success"),
+    USERNAME_ERROR(501, "usernameError"),
+    PASSWORD_ERROR(503, "passwordError"),
+    NOTLOGIN(504, "notLogin"),
+    SYSTEM_ERROR(40000, "systemError"),
+    USERNAME_USED(505, "userNameUsed");
 
     private Integer code;
     private String message;
+
     private ResultCodeEnum(Integer code, String message) {
         this.code = code;
         this.message = message;
     }
+
     public Integer getCode() {
         return code;
     }
+
     public String getMessage() {
         return message;
     }

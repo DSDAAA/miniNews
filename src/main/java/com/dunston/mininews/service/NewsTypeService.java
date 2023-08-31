b0d78f17834ef59e835572242011e572ca069a3d
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-31 16:05:27 +0800

    获取新闻类型接口

--- src/main/java/com/dunston/mininews/service/NewsTypeService.java
+++ src/main/java/com/dunston/mininews/service/NewsTypeService.java
@@ -2,12 +2,21 @@ package com.dunston.mininews.service;
 
 import com.dunston.mininews.domain.NewsType;
 import com.baomidou.mybatisplus.extension.service.IService;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
 
 /**
-* @author 13180
-* @description 针对表【news_type】的数据库操作Service
-* @createDate 2023-08-31 15:00:18
-*/
+ * @author dunston
+ * @description 针对表【news_type】的数据库操作Service
+ * @createDate 2023-08-31 15:00:18
+ */
 public interface NewsTypeService extends IService<NewsType> {
-
+    /**
+     * 查询所有type
+     *
+     * @param request
+     * @return
+     */
+    List<NewsType> findAllTypes(HttpServletRequest request);
 }

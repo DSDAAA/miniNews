51ffe6f8a6eb04e6399dde65caceceb3cb691afb
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-09-01 09:30:41 +0800

    完善功能

--- src/main/java/com/dunston/mininews/service/NewsHeadlineService.java
+++ src/main/java/com/dunston/mininews/service/NewsHeadlineService.java
@@ -2,12 +2,47 @@ package com.dunston.mininews.service;
 
 import com.dunston.mininews.domain.NewsHeadline;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.dunston.mininews.domain.request.NewsHeadlineRequest;
+import com.dunston.mininews.domain.request.NewsPageData;
+import com.dunston.mininews.domain.request.NewsPageRequest;
+
+import java.util.List;
 
 /**
-* @author 13180
-* @description 针对表【news_headline】的数据库操作Service
-* @createDate 2023-08-31 16:07:22
-*/
+ * @author 13180
+ * @description 针对表【news_headline】的数据库操作Service
+ * @createDate 2023-08-31 16:07:22
+ */
 public interface NewsHeadlineService extends IService<NewsHeadline> {
+    /**
+     * 新闻信息分页获取
+     *
+     * @param newsPageRequest
+     * @return
+     */
+    List<NewsPageData> findNewsPage(NewsPageRequest newsPageRequest);
 
+    /**
+     * 新闻脱敏化
+     *
+     * @param newsHeadline
+     * @return
+     */
+    NewsPageData getModelNews(NewsHeadline newsHeadline);
+
+    /**
+     * 显示头条新闻
+     *
+     * @param hid
+     * @return
+     */
+    NewsHeadlineRequest showHeadlineDetail(Integer hid);
+
+    /**
+     * 脱敏头条新闻
+     *
+     * @param newsHeadline
+     * @return
+     */
+    NewsHeadlineRequest getModelShowNews(NewsHeadline newsHeadline);
 }

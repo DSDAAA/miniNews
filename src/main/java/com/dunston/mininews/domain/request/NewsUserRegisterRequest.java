f85eac57451e64ff47d77a6c9ffc74ef769e3ab7
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-09-01 09:27:30 +0800

    说明注释

--- src/main/java/com/dunston/mininews/domain/request/NewsUserRegisterRequest.java
+++ src/main/java/com/dunston/mininews/domain/request/NewsUserRegisterRequest.java
@@ -4,6 +4,11 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * 用户注册封装类
+ *
+ * @author dunston
+ */
 @Data
 public class NewsUserRegisterRequest implements Serializable {
     private static final long serialVersionUID = 31912417163120793L;
